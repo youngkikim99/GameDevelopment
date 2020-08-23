f41e87dad256e3cd48f1855e41c143a91158d808
Young Ki Kim <13523877@student.uts.edu.au>
2020-08-23 23:55:30 +1000

    70 percent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
